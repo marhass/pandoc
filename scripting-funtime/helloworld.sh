2ceefdac89438756298fff944438043b244e8a3b
marhass <marhass@live.unc.edu>
2017-07-13 12:56:55 -0400

    class tutorial scripting

---
+++ scripting-funtime/helloworld.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# testing testing
+
+echo ""
+echo "Good morning, Marlee."
+echo ""
+echo "The date is `date --iso-8601`."
+echo ""
+echo "You are working as `whoami`."
+echo ""
+echo "Your working directory is `pwd`."
+echo ""
+echo "You have `ls -lah`"
+echo ""
+echo "Your IP is `hostname -i`"
+echo ""
